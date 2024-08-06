b5343ce36f1077fb40d31ada046e785db1dadfb3
michael <michael@miranda.works>
2024-08-06 12:33:16 +0200

    init

--- lib/easy_latex.dart
+++ lib/easy_latex.dart
@@ -1,1 +1,17 @@
 library easy_latex;
+
+import 'package:flutter/widgets.dart';
+
+class TestWidget extends StatelessWidget {
+  final double variable;
+
+  const TestWidget({
+    super.key,
+    required this.variable,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
